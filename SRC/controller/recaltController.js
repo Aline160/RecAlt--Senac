cacf85f94830a228eaa4d243708d5f6f7d610a36
edson040 <edsoncabral578@gmail.com>
2022-03-09 20:33:14 -0300

    ejs

--- SRC/controller/recaltController.js
+++ SRC/controller/recaltController.js
@@ -8,3 +8,24 @@ const livros=require('../model/recaltModel')
 module.exports = {
 
 }
+const res = require("express/lib/response");
+const fs = require("fs");
+
+const livros= require('../model/recaltController.js');
+const recalt = require("../model/recaltModel");
+
+const getAll=(req,res)=>{
+ recaltMongo.find({},function(err,livro){
+     if(err) {
+         res.status(500).send({ message: err.message })
+     }
+     res.status(200).render('index',{
+         livrosList:livro
+     })
+    })
+};
+
+
+module.exports = {
+     getAll,
+}

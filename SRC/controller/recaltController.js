1b806cb85e24f158de2839ff3b509c6df1fc4836
aline160 <alyne.p2@gmail.com>
2022-03-18 10:31:16 -0300

    put e delete

--- SRC/controller/recaltController.js
+++ SRC/controller/recaltController.js
@@ -36,8 +36,55 @@ const enviarLugares= (req,res)=>{
     };
 
 
+const putLugares = (req, res)=>{
+  const id = req.params.id;
+  try{
+
+    lugaresMongo.updateOne(
+      { id },
+      { $set: req.body },
+      function (err) {
+      res.status(201).send({ message: "Tarefa atualizada com sucesso!" });
+  })
+  } catch (err) {
+    return res.status(424).send({ message: err });
+  }
+}
+
+
+const atualizarLugares= (req,res)=>{
+  res.status(200).sendFile('paginas/Put.html',{root:__dirname})
+  };
+
+
+
+
+  const deleteLugares = (req, res) => {
+    const id = req.params.id;
+    try{
+      lugaresMongo.find({ id }, function(err, lugar){
+        if(lugaresMongo.length > 0){
+            lugaresMongo.deleteMany({ id }, function (err) {
+                if (!err) {
+                  res.status(200).send({ message: 'Tarefa removida com sucesso', status: "SUCCESS" })
+                }
+              })
+        }else res.status(200).send({ message: 'Não há tarefa para ser removida', status: "EMPTY" })
+    })
+}catch (err) {
+  console.log(err)
+  return res.status(424).send({ message: "Erro ao deletar o registro de tarefa" })
+}
+};
+
+
+
+
 module.exports = {
      getAll,
      postLugares,
-     enviarLugares
+     enviarLugares,
+     putLugares,
+     atualizarLugares,
+     deleteLugares
 }
--- SRC/routes/recaltRoutes.js
+++ SRC/routes/recaltRoutes.js
@@ -5,8 +5,14 @@ const controller = require('../controller/recaltController');
 
 route.get('/',controller.getAll);
 route.get('/enviar',controller.enviarLugares);
+route.get('/atualizar',controller.atualizarLugares);
+
 
 route.post('/enviar',controller.postLugares);
+
+route.put('/atualizar/:id',controller.putLugares);
+
+route.delete('/deletar/:id',controller.deleteLugares);
 
 
 module.exports= route
